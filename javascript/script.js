507de7781273870accbad35d53f87e5487b18af1
Suraj Sharma <80053118+SURAJSHARMA-ind@users.noreply.github.com>
2023-09-18 20:49:33 +0530

    inital_comit

--- javascript/script.js
+++ javascript/script.js
@@ -3,11 +3,15 @@
 var light = document.getElementById('light');
 
 light.onclick = function() {
+    console.log("u click on me");
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
-        light.src = "./icon/light.png";
+        light.src = "../resources/icons/light.png";
     }else{
-        light.src = "./icon/night-mode.png";
+        light.src = "../resources/icons/night-mode.png";
     }
+
 }
+
+
 // Dark mode code End here....
